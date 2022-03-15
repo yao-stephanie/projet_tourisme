c90bf9f4c97392b5144303aa506ab324c0a861f7
yao-stephanie <frondostephanie000@gmail.com>
2022-03-15 00:01:24 +0000

    localhost

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
+const PORT =5000;
+const router = require('./router/inscription')
+const router_inscription = require('./router/inscription');
 
+app.set('views','./views');
+app.set('view engine','ejs');
 
-// app.get('/',function(req , res) {
-//     res.send('hello world')
-// })
+app.use('/Public', express.static('public'));
+app.use("/",router_inscription);
+app.use("/",router);
+
+app.listen( PORT , ()=>{
+    console.log(`listening on port ${PORT}`)
+})
